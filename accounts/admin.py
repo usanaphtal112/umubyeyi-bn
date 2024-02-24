3f385c0a0578cb1d303ec3cdf2ec47bfa2fba68a
Naphtal USABYUWERA <usanaphtal112@gmail.com>
2024-02-25 03:32:33 +0530

    Fixed registration form

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,31 +1,58 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 from .models import CustomUser, Role
 
 
+class CustomUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = CustomUser
+        fields = "__all__"
+        exclude = ("username",)
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = CustomUser
+        fields = "__all__"
+        exclude = ("username",)
+
+
 class CustomUserAdmin(UserAdmin):
-    model = CustomUser
+    form = CustomUserChangeForm
+    add_form = CustomUserCreationForm
+
+    def get_fieldsets(self, request, obj=None):
+        fieldsets = super().get_fieldsets(request, obj)
+        # Remove the 'username' field from the fieldsets
+        fieldsets = list(fieldsets)
+        for fieldset in fieldsets:
+            if "username" in fieldset[1]["fields"]:
+                fieldset[1]["fields"] = tuple(
+                    field for field in fieldset[1]["fields"] if field != "username"
+                )
+        return fieldsets
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("phonenumber", "password1", "password2"),
+            },
+        ),
+    )
     list_display = (
-        "id",
         "phonenumber",
-        "username",
         "email",
         "firstname",
         "lastname",
-        "role",
-        "is_staff",
-        "is_profile_updated",
-        "country",
-        "city",
-        "address_line1",
-        "address_line2",
         "is_active",
-        "date_joined",
+        "is_staff",
+        "is_superuser",
     )
-    fieldsets = UserAdmin.fieldsets + (
-        ("Custom Fields", {"fields": ("phonenumber", "firstname", "lastname", "role")}),
-    )
+    search_fields = ("phonenumber", "email", "firstname", "lastname")
+    ordering = ("phonenumber",)
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,14 +1,9 @@
 from rest_framework import serializers
-
 from .models import Role
 
 
 class CustomUserSerializers(serializers.Serializer):
-    username = serializers.CharField(max_length=50)
     phonenumber = serializers.CharField(max_length=10)
-    # email = serializers.EmailField()
-    firstname = serializers.CharField(max_length=150)
-    lastname = serializers.CharField(max_length=150)
     password = serializers.CharField(write_only=True)
     confirm_password = serializers.CharField(write_only=True)
 
